0cf9933546ccdda82adc242f403173be14e75aa2
Dice0907 <dais.f07@gmail.com>
2019-05-29 09:46:26 +0900

    message_fix

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,7 +2,7 @@ class Api::MessagesController < ApplicationController
   def index
     respond_to do |format| 
       format.html 
-      format.json { @new_message = Message.where('id > ?', params[:id]) } 
+      format.json { @new_message = Message.where('id > ?', params[:message][:id]) } 
     end
   end
 end
